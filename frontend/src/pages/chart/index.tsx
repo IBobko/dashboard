203c7b28d0c8a986856efc8e2cd9a95483094db3
Igor Bobko <iibobko@gmail.com>
2023-07-15 15:39:08 -0700

    Refactor chart page component to use dynamic grid size
    Summary:
    - Updated the `useState` hook to use the `gridSize` variable for initializing the boolean matrix.
    - Modified the `handleInputChange` function to check the input length based on the `gridSize` variable.
    - Adjusted the loops in the `handleInputChange` function and `redraw` function to use the `gridSize` variable.
    - Removed the hardcoded value of `grid` and replaced it with the `gridSize` variable in the `redraw` function.
    - Updated the canvas size in the JSX code to reflect the new grid size.
    - Removed the unused `renderCount` state variable and its usage.
    - Modified the `onClick` event handler of the "Redraw" button to update the `gridSize` state instead of toggling boolean matrix values.
    
    Details:
    - Replaced hardcoded values related to the grid size with the `gridSize` variable.
    - Improved code clarity and removed unnecessary repetitions.

--- frontend/src/pages/chart/index.tsx
+++ frontend/src/pages/chart/index.tsx
@@ -3,24 +3,24 @@ import { Box, Input, Button, VStack } from "@chakra-ui/react";
 
 function ChartPage() {
     const canvasRef = useRef(null);
-    const [renderCount, setRenderCount] = useState(0);
+    const [gridSize, setGridSize] = useState(8);
     const [boolMatrix, setBoolMatrix] = useState(() => {
-        return new Array(8).fill(null).map(() => new Array(8).fill(false));
+        return new Array(gridSize).fill(null).map(() => new Array(gridSize).fill(false));
     });
 
     const handleInputChange = (event) => {
         const input = event.target.value;
 
-        if (input.length !== 64 || !/^[01]+$/.test(input)) {
+        if (input.length !== gridSize * gridSize || !/^[01]+$/.test(input)) {
             // Input is invalid, ignore it
             return;
         }
         // Convert the input string to a 2D array
         const newMatrix = [];
-        for (let i = 0; i < 8; i++) {
+        for (let i = 0; i < gridSize; i++) {
             const row = [];
-            for (let j = 0; j < 8; j++) {
-                row.push(input[i * 8 + j] === '1');
+            for (let j = 0; j < gridSize; j++) {
+                row.push(input[i * gridSize + j] === '1');
             }
             newMatrix.push(row);
         }
@@ -30,15 +30,14 @@ function ChartPage() {
     const redraw = useCallback(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
-        const grid = 8;
         const cellSize = 20; // Можно менять для изменения размера сетки
 
         // Clear the previous grid
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         // Рисуем сетку
-        for (let x = 0; x <= grid; x++) {
-            for (let y = 0; y <= grid; y++) {
+        for (let x = 0; x <= gridSize; x++) {
+            for (let y = 0; y <= gridSize; y++) {
                 context.beginPath();
                 context.rect(x * cellSize, y * cellSize, cellSize, cellSize);
                 context.lineWidth = 1;
@@ -52,23 +51,20 @@ function ChartPage() {
                 }
             }
         }
-    }, [boolMatrix]);
+    }, [boolMatrix, gridSize]);
 
     useEffect(() => {
         redraw();
-    }, [renderCount, redraw]);
+    }, [boolMatrix, redraw]);
 
     return (
         <VStack spacing={5}>
-            <Box boxSize="160px">
-                <canvas ref={canvasRef} width={160} height={160} />
+            <Box boxSize={`${gridSize * 20}px`}>
+                <canvas ref={canvasRef} width={gridSize * 20} height={gridSize * 20} />
             </Box>
             <Input type="text" onChange={handleInputChange} />
             <Button colorScheme="blue" onClick={() => {
-                setRenderCount(prevCount => prevCount + 1);
-                // Здесь вы можете обновить булеву матрицу по своему усмотрению
-                // Для примера, я просто переключаю значения в матрице
-                setBoolMatrix(boolMatrix.map(row => row.map(cell => !cell)));
+                setGridSize(16);
             }}>Redraw</Button>
         </VStack>
     );
