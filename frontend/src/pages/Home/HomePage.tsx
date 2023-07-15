a8e3b462f0068e3b8ff4db22f8433cfbc7d11837
Igor Bobko <iibobko@gmail.com>
2023-07-15 14:27:24 -0700

    Refactor HomePage component to use Chakra UI components and update layout
    The HomePage component in the frontend/src/pages/Home/HomePage.tsx file has been refactored to use Chakra UI components. The layout has been updated to include a dashboard heading and links to different pages, such as MNIST, Telegram, and Chart. The changes improve the visual appearance and user experience of the homepage.

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Box, Button, Flex, Heading, Link } from "@chakra-ui/react";
 
 interface Item {
   companyName: string;
@@ -15,10 +16,25 @@ function HomePage() {
   }, []);
 
   return (
-    <div>
-      <h1>New Page</h1>
-
-    </div>
+    <Box>
+      <Flex bg="teal.500" p={4} color="white">
+        <Heading flexGrow={1}>Igor Bobko's dashboard</Heading>
+      </Flex>
+      <Box p={4}>
+        <Heading as="h2" size="xl" mb={4}>
+          Welcome!
+        </Heading>
+        <Link href="/mnist" mr={2}>
+          MNIST
+        </Link>
+        <Link href="/telegram">
+          Telegram
+        </Link>
+        <Link href="/chart">
+          Chart
+        </Link>
+      </Box>
+    </Box>
   );
 }
 
