e3d124acc00fadba188ddd718faf64f0960ddf60
Igor Bobko <iibobko@gmail.com>
2023-07-15 15:33:45 -0700

    Implement input handling and improve UI layout
    - Added import statement for Chakra UI components in the ChartPage component
    - Implemented handleInputChange function to handle changes in the input field
    - Updated the setBoolMatrix function to convert the input string to a 2D boolean matrix
    - Added an Input component for user input
    - Updated the layout of the UI using Chakra UI components, including Box, Input, and Button
    - Replaced the canvas element with a Box component to wrap it for better styling
    - Updated the onClick event handler for the Button component to increment the render count and update the boolean matrix accordingly
    - Added spacing between components using the VStack component

--- frontend/src/pages/chart/index.tsx
+++ frontend/src/pages/chart/index.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect, useState, useCallback } from 'react';
+import { Box, Input, Button, VStack } from "@chakra-ui/react";
 
 function ChartPage() {
     const canvasRef = useRef(null);
@@ -6,6 +7,25 @@ function ChartPage() {
     const [boolMatrix, setBoolMatrix] = useState(() => {
         return new Array(8).fill(null).map(() => new Array(8).fill(false));
     });
+
+    const handleInputChange = (event) => {
+        const input = event.target.value;
+
+        if (input.length !== 64 || !/^[01]+$/.test(input)) {
+            // Input is invalid, ignore it
+            return;
+        }
+        // Convert the input string to a 2D array
+        const newMatrix = [];
+        for (let i = 0; i < 8; i++) {
+            const row = [];
+            for (let j = 0; j < 8; j++) {
+                row.push(input[i * 8 + j] === '1');
+            }
+            newMatrix.push(row);
+        }
+        setBoolMatrix(newMatrix);
+    };
 
     const redraw = useCallback(() => {
         const canvas = canvasRef.current;
@@ -39,15 +59,18 @@ function ChartPage() {
     }, [renderCount, redraw]);
 
     return (
-        <div>
-            <canvas ref={canvasRef} width={160} height={160} />
-            <button onClick={() => {
+        <VStack spacing={5}>
+            <Box boxSize="160px">
+                <canvas ref={canvasRef} width={160} height={160} />
+            </Box>
+            <Input type="text" onChange={handleInputChange} />
+            <Button colorScheme="blue" onClick={() => {
                 setRenderCount(prevCount => prevCount + 1);
                 // Здесь вы можете обновить булеву матрицу по своему усмотрению
                 // Для примера, я просто переключаю значения в матрице
                 setBoolMatrix(boolMatrix.map(row => row.map(cell => !cell)));
-            }}>Redraw</button>
-        </div>
+            }}>Redraw</Button>
+        </VStack>
     );
 }
 
